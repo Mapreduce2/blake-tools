77fc7b60e68bbc8a0f83894a032d84d64d0d4949
Blake <Blake.Russo@live.com>
2019-02-18 16:43:28 -0500

    Update cluster-ssh.sh

--- cluster-ssh.sh
+++ cluster-ssh.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-#owner Blake Russo TUM (blruss)
-#requires creation of hostlist /etc/gridhosts. 
+#Owner : Blake Russo 
+#Requires list of hosts in file named /etc/gridhosts.
+
 if [ -f "/etc/gridhosts" ]
     then
-       for x in `cat /etc/gridhosts`; do scp $1 $x:$2 ; done
+        for x in `cat /etc/gridhosts | grep -v $HOSTNAME `; do ssh $x $1 ; done
     else
         echo
         echo "ERROR: /etc/gridhosts NOT FOUND!"
         echo "A list of hosts to ssh to is required, please create /etc/gridhosts file"
-        echo "Unable to scp file."
 fi
+
