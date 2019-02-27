36fcb22bec7572d99a4b6bc1c0c28d56b7a57ba9
Blake <Blake.Russo@live.com>
2019-02-27 10:30:24 -0500

    Update snippets.sh

--- docker/snippets.sh
+++ docker/snippets.sh
@@ -18,3 +18,20 @@ open my $pipe, '|chpasswd' or die "can't open pipe: $!";
 print {$pipe} "$username:$password";
 close $pipe
 
+#remove dangling failed docker images/containers
+
+docker rmi $(docker images -q -f dangling=true)
+ 
+#login to shell
+
+docker exec -it container-id bash
+
+#run container with port forwarding
+
+docker run -d -p 8080:8080 image:tag 
+
+#run basic web app container from k8s
+
+kubectl run  --image=docker-image-name:tag --port=8080 k8spodname
+
+#still must expose 
